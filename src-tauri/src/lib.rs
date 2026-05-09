af324026b779cb62147abff6c249742429b5fe18
Frederick Jerusha <frederickjerusha@gmail.com>
2026-05-09 23:09:53 +0700

    feat: scaffold src-tauri (Cargo.toml, lib.rs, main.rs)

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,18 @@
+mod modules;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_os::init())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(tauri_plugin_log::log::LevelFilter::Info)
+                .build(),
+        )
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src-tauri/src/modules/mod.rs
@@ -0,0 +1,2 @@
+// Modules are added by subsequent stages:
+//   pty (Stage B), fs (Stage E), secrets (Stage G), shell (Stage I)
